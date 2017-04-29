ce772944544b831183da959008ed427b94cb951c
tshepontlhokoa <tshepo.ntlhokoa@gmail.com>
2017-04-29 20:32:58 +0200

    first commit

---
+++ code/after/step 1 (using bower to manage third-party libs)/js/app/app.js
@@ -0,0 +1,8 @@
+﻿window.Application = Ember.Application.create({});
+
+Application.Router.map(function () {
+    this.resource('index', { path: "/" });
+    this.resource('about', { path: "/about" });
+    this.resource('post', { path: "/post" });
+    this.resource('contact', { path: "/contact" });
+});
---
+++ code/before/gulpfile.js
@@ -0,0 +1,39 @@
+﻿var gulp = require('gulp');
+var handlebars = require('gulp-ember-handlebars');
+var print = require('gulp-print');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+
+var templatesHandlebars = './js/app/**/*.handlebars';
+var emberTemplates = './js/app/**/*.template.js';
+var templatesOutDir = './js/app';
+
+gulp.task('cleanup', function () {   
+    return gulp.src([emberTemplates], { read: false })
+               .pipe(print(function (name) {
+                   return "DELETED FILE: " + name;
+               }))
+               .pipe(clean());
+
+});
+
+gulp.task('compiletemplates',['cleanup'], function () {
+    return gulp.src(templatesHandlebars)
+        .pipe(print(function (name) {
+            return "INPUT: " + name;
+        }))
+        .pipe(handlebars({
+            outputType: 'browser',
+            processName: function (name) {
+                return name.substr(name.lastIndexOf('/') + 1)
+                           .replace('.handlebars', '');
+            }
+        }))
+        .pipe(rename({ suffix: '.template' }))
+        .pipe(print(function (name) {
+            return "OUTPUT: " + name;
+        }))
+        .pipe(gulp.dest(templatesOutDir));
+});
+
+gulp.task('default', ['compiletemplates']);
